b38b5e99aaf67f962283f528ecdfc0fa1d5a6d95
RAYAN AREEB KHANDKER <rayanareeb2@gmail.com>
2026-02-27 14:56:47 +0600

    Create insert_sort.py
    Insertion Sort Algorithm in Ascending Order

---
+++ insert_sort.py
@@ -0,0 +1,12 @@
+Values = list(map(int, input("Enter values, separating them by comma, to be sorted: ").split(','))) #Prompt user to input values in single line and convert to array of integers
+Length = len(Values) 
+
+for Index in range(1, Length): #Insertion sort algorithm
+    Key = Index
+    while Key > 0 and Values[Key - 1] > Values[Key]:
+        Temp = Values[Key - 1]
+        Values[Key - 1] = Values[Key]
+        Values[Key] = Temp
+        Key = Key - 1
+
+print(Values) #Output array
