44adca952c172accbaeec93af13bba8c6695a822
RAYAN AREEB KHANDKER <rayanareeb2@gmail.com>
2026-02-27 13:07:42 +0600

    Create binary_search.py
    Binary Search Algorithm for Array with 15 random and unique integers

---
+++ binary_search.py
@@ -0,0 +1,29 @@
+import random #Imports Random library
+
+Array = []
+for x in range(15): #Appending array with 15 random numbers
+    Number = random.randint(0, 51)
+    if Number not in Array: #Ensures each integer inputted is unique
+        Array.append(Number)
+Array.sort() #Sorts array in ascending order by default
+
+#Binary Search algorithm
+Low = 0
+High = len(Array)
+Found = False
+
+SearchValue = int(input("Enter the value you want to search: "))
+
+while Found == False and Low <= High:
+    Mid = int((Low + High) / 2)
+    if Array[Mid] > SearchValue:
+        High = Mid - 1
+    elif Array[Mid] < SearchValue:
+        Low = Mid + 1
+    else:
+        Found = True
+
+if Found:
+    print(f"Value has been found at index {Mid}")
+else:
+    print("Value is not in array")
